b1a385b6fa75de86a66089691e89a6640d9c92d3
Warren Wong <wongwarren@hotmail.com>
2021-05-14 12:22:53 -0700

    Initial add

---
+++ src/main/java/scratchproject/App.java
@@ -0,0 +1,15 @@
+package scratchproject;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println(App.getMessage());
+	}
+	
+	public static String getMessage()
+	{
+		return "I am alive!";
+	}
+}
---
+++ src/test/java/scratchproject/AppTest.java
@@ -0,0 +1,13 @@
+package scratchproject;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest {
+
+	@Test
+	public void testOne() {
+		Assert.assertEquals("I am alive!", App.getMessage());
+		
+	}
+	
+}
